0c26b2b8009a6999cd2da0be8e84a023b5d9d57d
Davidcito12 <92931033+Davidcito12@users.noreply.github.com>
2022-03-25 01:32:52 -0500

    Intento 2

---
+++ televisores/marca.py
@@ -0,0 +1,9 @@
+class Marca:
+    def __init__(self,Nombre):
+        self._nombre = Nombre
+
+    def getMarca(self):
+        return self._nombre
+
+    def setMarca(self,Nombre):
+        self._nombre = Nombre
